c3a39b95c1f7882deb9e9f306bb04079eb669df1
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-06-22 14:17:54 +0200

    Anulados y ciclo inverso del toc-paytef OK

--- dist/paytef/paytef.class.d.ts
+++ dist/paytef/paytef.class.d.ts
@@ -2,17 +2,15 @@
 import { TicketsInterface } from "src/tickets/tickets.interface";
 import { CestasInterface } from "src/cestas/cestas.interface";
 import { Socket } from 'dgram';
+import { Respuesta } from "./paytef.interface";
 declare class PaytefClass {
     getTotal(cesta: CestasInterface): number;
+    cancelarOperacion(): void;
+    iniciarDatafono(idTicket: number, total: number, client: Socket): void;
     iniciarTransaccion(client: Socket, idCliente: string, idCesta: number): Promise<void>;
-    consultarEstadoOperacion(client: Socket): Promise<void>;
-    cerrarTicket(nuevoTicket: TicketsInterface): Promise<{
-        error: boolean;
-        mensaje?: undefined;
-    } | {
-        error: boolean;
-        mensaje: string;
-    }>;
+    anularOperacion(idTicket: number, client: Socket): void;
+    consultarEstadoOperacion(client: Socket, idTicket: number): Promise<void>;
+    cerrarTicket(nuevoTicket: TicketsInterface): Promise<Respuesta>;
 }
 declare const paytefInstance: PaytefClass;
 export { paytefInstance };
