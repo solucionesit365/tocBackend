bf675e1dd6e08fbfac1ded95cbc458866e738b7b
dobleamarilla <ezequielucho@gmail.com>
2022-08-24 11:06:44 +0200

    Modificaciones de Paytef (bloqueados)

--- src/paytef/paytef.class.ts
+++ src/paytef/paytef.class.ts
@@ -1,22 +1,22 @@
-import axios from 'axios';
-import { SocketGateway } from '../sockets.gateway';
-import { movimientosInstance } from '../movimientos/movimientos.clase';
-import { parametrosInstance } from 'src/parametros/parametros.clase';
-import { trabajadoresInstance } from 'src/trabajadores/trabajadores.clase';
-import { ticketsInstance } from 'src/tickets/tickets.clase';
-import { cestas } from 'src/cestas/cestas.clase';
-import { TicketsInterface } from 'src/tickets/tickets.interface';
-import { CestasInterface } from 'src/cestas/cestas.interface';
-import { transaccionesInstance } from 'src/transacciones/transacciones.class';
-import { nuevoTicket } from 'src/tickets/tickets.mongodb';
-import { UtilesModule } from 'src/utiles/utiles.module';
-import { LogsClass } from 'src/logs/logs.class';
-import { TransaccionesInterface } from 'src/transacciones/transacciones.interface';
-import { Socket } from 'dgram';
-import { Respuesta } from './paytef.interface';
+import axios from "axios";
+import { SocketGateway } from "../sockets.gateway";
+import { movimientosInstance } from "../movimientos/movimientos.clase";
+import { parametrosInstance } from "src/parametros/parametros.clase";
+import { trabajadoresInstance } from "src/trabajadores/trabajadores.clase";
+import { ticketsInstance } from "src/tickets/tickets.clase";
+import { cestas } from "src/cestas/cestas.clase";
+import { TicketsInterface } from "src/tickets/tickets.interface";
+import { CestasInterface } from "src/cestas/cestas.interface";
+import { transaccionesInstance } from "src/transacciones/transacciones.class";
+import { nuevoTicket } from "src/tickets/tickets.mongodb";
+import { UtilesModule } from "src/utiles/utiles.module";
+import { LogsClass } from "src/logs/logs.class";
+import { TransaccionesInterface } from "src/transacciones/transacciones.interface";
+import { Socket } from "dgram";
+import { Respuesta } from "./paytef.interface";
 
 function limpiarNombreTienda(cadena: string) {
-  const devolver = Number(cadena.replace(/\D/g, ''));
+  const devolver = Number(cadena.replace(/\D/g, ""));
   if (isNaN(devolver) == false) {
     return devolver;
   } else {
@@ -34,17 +34,30 @@ class PaytefClass {
   }
 
   /* Parámetros de entrada OK */
-  async iniciarTransaccion(client: Socket, idCliente: string, idCesta: number): Promise<void> {
+  async iniciarTransaccion(
+    client: Socket,
+    idCliente: string,
+    idCesta: number
+  ): Promise<void> {
     try {
-      const idTrabajadorActivo = await trabajadoresInstance.getCurrentIdTrabajador();
+      const idTrabajadorActivo =
+        await trabajadoresInstance.getCurrentIdTrabajador();
       if (idTrabajadorActivo != null) {
         const cesta = await cestas.getCestaByID(idCesta);
         if (cesta != null) {
           const total = this.getTotal(cesta);
           if (cesta.lista.length > 0 && total > 0) {
-            const nuevoTicket = ticketsInstance.generarObjetoTicket((await ticketsInstance.getUltimoTicket()) + 1, total, cesta, 'TARJETA', idTrabajadorActivo, idCliente);
+            const nuevoTicket = ticketsInstance.generarObjetoTicket(
+              (await ticketsInstance.getUltimoTicket()) + 1,
+              total,
+              cesta,
+              "TARJETA",
+              idTrabajadorActivo,
+              idCliente
+            );
             nuevoTicket.bloqueado = true;
-            if (await paytefInstance.cerrarTicket(nuevoTicket)) { // Ticket creado
+            if (await paytefInstance.cerrarTicket(nuevoTicket)) {
+              // Ticket creado
               this.iniciarDatafono(nuevoTicket._id, total, client, cesta._id);
             } else {
               throw Error("Error, no se ha podido cerrar el ticket");
@@ -60,16 +73,16 @@ class PaytefClass {
       }
     } catch (err) {
       this.cancelarOperacion();
-      client.emit('consultaPaytef', { error: true, mensaje: err.message });
+      client.emit("consultaPaytef", { error: true, mensaje: err.message });
     }
   }
 
   async cerrarTicket(nuevoTicket: TicketsInterface): Promise<boolean> {
     try {
       if (await ticketsInstance.insertarTicket(nuevoTicket)) {
-          if (await parametrosInstance.setUltimoTicket(nuevoTicket._id)) {
-            return true;
-          }
+        if (await parametrosInstance.setUltimoTicket(nuevoTicket._id)) {
+          return true;
+        }
         this.anularOperacion(nuevoTicket._id, "cerrarTicket()");
         return false;
       }
@@ -83,38 +96,55 @@ class PaytefClass {
   /* Cancela la operación en el propio datáfono */
   cancelarOperacion() {
     const params = parametrosInstance.getParametros();
-    axios.post(`http://${params.ipTefpay}:8887/pinpad/cancel`, { 'pinpad': '*' });
+    axios.post(`http://${params.ipTefpay}:8887/pinpad/cancel`, { pinpad: "*" });
   }
 
   /* Anula el ticket creado (por algún error). NO detiene el ciclo (sin respuesta al cliente) */
-  anularOperacion(idTicket: number, msj = '') {
-    ticketsInstance.borrarTicket(idTicket).then((resAnulacion) => {
-      if (resAnulacion == false) {
-        LogsClass.newLog('Error nuevo grave', `Ticket debía ser borrado pero no se ha podido: idTicket: ${idTicket} tiemstamp: ${Date.now()} y viene de ${msj}`);
-      }
-    }).catch((err) => {
-      console.log(err);
-      LogsClass.newLog('Traza paytef', `anularOperacion(${idTicket}, client, msj) entra en su catch. timestamp ${Date.now()} y otra vez el idTicket: ${idTicket}`);
-    });
+  anularOperacion(idTicket: number, msj = "") {
+    ticketsInstance
+      .borrarTicket(idTicket)
+      .then((resAnulacion) => {
+        if (resAnulacion == false) {
+          LogsClass.newLog(
+            "Error nuevo grave",
+            `Ticket debía ser borrado pero no se ha podido: idTicket: ${idTicket} tiemstamp: ${Date.now()} y viene de ${msj}`
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        LogsClass.newLog(
+          "Traza paytef",
+          `anularOperacion(${idTicket}, client, msj) entra en su catch. timestamp ${Date.now()} y otra vez el idTicket: ${idTicket}`
+        );
+      });
   }
 
-  async iniciarDatafono(idTicket: number, total: number, client: Socket, idCesta: number): Promise<void> {
+  async iniciarDatafono(
+    idTicket: number,
+    total: number,
+    client: Socket,
+    idCesta: number
+  ): Promise<void> {
     try {
       const params = parametrosInstance.getParametros();
       if (UtilesModule.checkVariable(params.ipTefpay)) {
-        const respuestaPaytef: any = await axios.post(`http://${params.ipTefpay}:8887/transaction/start`, {
-          pinpad: '*',
-          opType: 'sale',
-          createReceipt: true,
-          executeOptions: {
-            method: 'polling',
-          },
-          language: 'es',
-          requestedAmount: Math.round(total * 100),
-          requireConfirmation: false,
-          transactionReference: idTicket,
-          showResultSeconds: 5,
-        });
+        const respuestaPaytef: any = await axios.post(
+          `http://${params.ipTefpay}:8887/transaction/start`,
+          {
+            pinpad: "*",
+            opType: "sale",
+            createReceipt: true,
+            executeOptions: {
+              method: "polling",
+            },
+            language: "es",
+            requestedAmount: Math.round(total * 100),
+            requireConfirmation: false,
+            transactionReference: idTicket,
+            showResultSeconds: 5,
+          }
+        );
         if (respuestaPaytef.data.info.started) {
           // Arranca el ciclo de comprobaciones
           this.consultarEstadoOperacion(client, idTicket, total, idCesta);
@@ -127,8 +157,8 @@ class PaytefClass {
     } catch (err) {
       console.log(err);
       this.cancelarOperacion();
-      this.anularOperacion(idTicket, 'Backend: ' + err.message);
-      client.emit('consultaPaytef', {
+      this.anularOperacion(idTicket, "Backend: " + err.message);
+      client.emit("consultaPaytef", {
         error: false,
         mensaje: "Backend: " + err.message,
       });
@@ -136,62 +166,51 @@ class PaytefClass {
   }
 
   /* Función recursiva y asíncrona */
-  async consultarEstadoOperacion(client: Socket, idTicket: number, total: number, idCesta: number): Promise<void> {
+  async consultarEstadoOperacion(
+    client: Socket,
+    idTicket: number,
+    total: number,
+    idCesta: number
+  ): Promise<void> {
     try {
       const ipDatafono = parametrosInstance.getParametros().ipTefpay;
-      const resEstadoPaytef: any = await axios.post(`http://${ipDatafono}:8887/transaction/poll`, {
-        pinpad: '*'
-      });
+      const resEstadoPaytef: any = await axios.post(
+        `http://${ipDatafono}:8887/transaction/poll`,
+        {
+          pinpad: "*",
+        }
+      );
 
       /* ¿Ya existe el resultado de PayTef? */
       if (UtilesModule.checkVariable(resEstadoPaytef.data.result)) {
-        if (Number(resEstadoPaytef.data.result.transactionReference) == idTicket) {
-          if (resEstadoPaytef.data.result.approved) {
-            await ticketsInstance.desbloquearTicket(idTicket);
-            await movimientosInstance.nuevaSalida(total, 'Targeta', 'TARJETA', false, idTicket);
-            await cestas.borrarCesta(idCesta);
-            client.emit('consultaPaytef', { // Operación aprobada. Todo OK
-              error: false,
-            });
-          } else {
-            throw Error("Operación denegada por PayTef");
-          }
-        } else if (resEstadoPaytef.data.result.approved) {
-          LogsClass.newLog('Error nuevo (sin referencia)', `Ticket aprobado por PayTef y creado en el toc, pero no coincide la referencia: referenciaPaytef: ${Number(resEstadoPaytef.data.result.transactionReference)} idTicketFuncion: ${idTicket} timestamp: ${Date.now()}`);
+        if (resEstadoPaytef.data.result.approved) {
           await ticketsInstance.desbloquearTicket(idTicket);
-          await movimientosInstance.nuevaSalida(total, 'Targeta', 'TARJETA', false, idTicket);
+          await movimientosInstance.nuevaSalida(
+            total,
+            "Targeta",
+            "TARJETA",
+            false,
+            idTicket
+          );
           await cestas.borrarCesta(idCesta);
-          client.emit('consultaPaytef', { // Operación aprobada. Todo OK
+          client.emit("consultaPaytef", {
+            // Operación aprobada. Todo OK
             error: false,
           });
         } else {
-          throw Error("Operación denegada por PayTef. 2");
+          throw Error("Operación denegada por PayTef");
         }
-      } else { // Aún no existe el resultado, pero sí el estado del datáfono
-        switch (resEstadoPaytef.data.info.transactionStatus) {
-          case "cancelling": throw Error("Operación cancelada"); break;
-          case "none":
-          case "starting":
-          case "waitingForUser":
-          case "dcc":
-          case "signature":
-          case "processing":
-          case "waitingConfirmation":
-          case "finished":
-            await new Promise((r) => setTimeout(r, 1000));
-            this.consultarEstadoOperacion(client, idTicket, total, idCesta); break;
-          default:
-            LogsClass.newLog("warning importante", `¡info.transactionStatus: (${resEstadoPaytef.data.info.transactionStatus}) no tiene ningún valor de la documentación! - idTicket: ${idTicket} timestamp: ${Date.now()}`);
-            throw Error("¡No existe info en PayTef! Ver log warning importante");
-        }
+      } else {
+        await new Promise((r) => setTimeout(r, 1000));
+        this.consultarEstadoOperacion(client, idTicket, total, idCesta);
       }
     } catch (err) {
       this.cancelarOperacion();
       this.anularOperacion(idTicket, err.message);
-      LogsClass.newLog("Error backend paytefClass consultarEstadoOperacion", err.message);
+      LogsClass.newLog(idTicket, err.message);
       client.emit("consultaPaytef", {
         error: true,
-        mensaje: "Backend: " + err.message
+        mensaje: "Backend: " + err.message,
       });
     }
   }
