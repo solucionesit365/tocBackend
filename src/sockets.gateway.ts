ef1e5ffb4f051f908ed78342167ee499c6b867ab
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-10-07 15:52:46 +0200

    Update sockets.gateway.ts

--- src/sockets.gateway.ts
+++ src/sockets.gateway.ts
@@ -1,7 +1,10 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { cajaInstance } from "./caja/caja.clase";
 import { cestasInstance } from "./cestas/cestas.clase";
 import { logger } from "./logger";
+import { parametrosInstance } from "./parametros/parametros.clase";
+import { ticketsInstance } from "./tickets/tickets.clase";
 import { trabajadoresInstance } from "./trabajadores/trabajadores.clase";
 
 const httpServer = createServer();
@@ -27,6 +30,29 @@ io.on("connection", (socket) => {
   socket.on("cargarCestas", async (data) => {
     try {
       socket.emit("cargarCestas", await cestasInstance.getAllCestas());
+    } catch (err) {
+      logger.Error(err);
+      console.log(err);
+    }
+  });
+
+  /* Eze 4.0 */
+  socket.on("cargarParametros", async (data) => {
+    try {
+      socket.emit("cargarParametros", await parametrosInstance.getParametros());
+    } catch (err) {
+      logger.Error(err);
+      console.log(err);
+    }
+  });
+
+  /* Eze 4.0 */
+  socket.on("cargarVentas", async (data) => {
+    try {
+      if (await cajaInstance.cajaAbierta()) {
+        const caja = await cajaInstance.getInfoCajaAbierta();
+        socket.emit("cargarVentas", await ticketsInstance.getTicketsIntervalo(caja.inicioTime, Date.now()));
+      }      
     } catch (err) {
       logger.Error(err);
       console.log(err);
