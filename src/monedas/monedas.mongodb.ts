20217b3ce4e414d6d53fcbdf3cd961b697d7e6ca
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-10-28 13:02:29 +0200

    Update monedas.mongodb.ts

--- src/monedas/monedas.mongodb.ts
+++ src/monedas/monedas.mongodb.ts
@@ -1,13 +1,19 @@
-import { MonedasInterface } from 'src/caja/caja.interface';
-import {conexion} from '../conexion/mongodb';
+import { MonedasInterface } from "src/caja/caja.interface";
+import { conexion } from "../conexion/mongodb";
 import { logger } from "../logger";
 
-export async function setMonedas(data: MonedasInterface["array"]): Promise<boolean> {
+export async function setMonedas(
+  data: MonedasInterface["array"]
+): Promise<boolean> {
   try {
     const database = (await conexion).db("tocgame");
     const monedas = database.collection("monedas");
-    const resultado = await monedas.updateOne({_id: "INFO_MONEDAS"}, { $set: data }, { upsert: true });
-    return (resultado.acknowledged && resultado.modifiedCount > 0);
+    const resultado = await monedas.updateOne(
+      { _id: "INFO_MONEDAS" },
+      { $set: data },
+      { upsert: true }
+    );
+    return resultado.acknowledged && resultado.modifiedCount > 0;
   } catch (err) {
     logger.Error(97, err);
     return false;
